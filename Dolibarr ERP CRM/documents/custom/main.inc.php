9f0775456b4ec884f7ea12ed936c134e3f49518b
AfaafZaid <91932809+AfaafZaid@users.noreply.github.com>
2021-10-28 15:18:41 +0300

    Add files via upload

---
+++ Dolibarr ERP CRM/documents/custom/main.inc.php
@@ -0,0 +1,4 @@
+<?php
+// Wrapper to include main into htdocs
+include_once 'C:/dolibarr3/www/OuRdolibarr/htdocs/main.inc.php';
+?>
---
+++ Dolibarr ERP CRM/documents/custom/master.inc.php
@@ -0,0 +1,4 @@
+<?php
+// Wrapper to include master into htdocs
+include_once 'C:/dolibarr3/www/OuRdolibarr/htdocs/master.inc.php';
+?>
